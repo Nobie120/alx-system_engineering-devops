498ea30b98f4ec28822541f6f85b10e0cc1c82e6
Nobie120 <n12507400@gmail.com>
2024-06-19 08:49:44 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+	"""Return the nuber of subscribbers of a givn subreddit"""
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {
+	    "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+	}
+	response = requests.get(url, headers=headers, allow_redirects=False)
+	if response.status_code == 200:
+		data = response.json()
+		subscribers = data['data']['subscribers']
+		return subscribers
+	else:
+		return 0
